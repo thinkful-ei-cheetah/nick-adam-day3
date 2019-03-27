2daf44ea63ff20396dab8e3c1ee6991ae56879b1
Nick Lee <leexnick@gmail.com>
2019-03-27 09:43:26 -0700

    update object-drills-2

--- object-drills2.js
+++ object-drills2.js
@@ -1,16 +1,56 @@
 'use strict';
 function makeStudentsReport(data){
-    let anArray = [];
-    for (let i = 0; i < data.length; i++){
-        //console.log(data[i]);
-        anArray.push(`${data[i].name}: ${data[i].grade}`);
-    }
-   //console.log(anArray);
-    return anArray;
+  let anArray = [];
+  for (let i = 0; i < data.length; i++){
+    //console.log(data[i]);
+    anArray.push(`${data[i].name}: ${data[i].grade}`);
+  }
+  //console.log(anArray);
+  return anArray;
 }
 
 function enrollInSummerSchool(students){
-    for(let i = 0: i < students.length: i++){
-        Object.assign(students[i],{status: 'In Summer school'});
+  const resultArr = [];
+
+  for(let i = 0; i < students.length; i++){
+    resultArr.push(Object.assign(students[i],{status: 'In Summer school'}));
+  }
+
+  return resultArr;
+}
+
+let arr = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology'
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics'
+  }
+];
+
+// console.log(enrollInSummerSchool(arr));
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
+  
+function findById(items, idNum) {
+//   let resultArr = [];
+
+  for(let i=0; i<items.length; i++) {
+    if(items[i].id === idNum) {
+      return items[i];
     }
+  }
+}
+
+// console.log(findById(scratchData, 22));
+
+function validateKeys(object, expectedKeys) {
+    
 }
